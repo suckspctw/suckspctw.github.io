fdfbf9e6888eebd2f15ecc7c347e2e4cf37a1c08
suckspctw <suckspctw@gmail.com>
2024-10-31 15:19:07 +0800

    u

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
 import defaultArticles from '@/assets/json/default-articles.json'
 import AppSection from '@/components/AppSection.vue'
+
+const title = '原價屋消費爭議記錄'
+
+useHead({
+  title,
+  meta: [
+    { name: 'keywords', content: '原價屋,二手屋,瑕疵屋,高價屋' },
+    { name: 'description', content: '千錯萬錯都是消費者的錯，原價屋跟原廠永遠不會錯' }
+  ]
+})
 </script>
 
 <template>
   <div class="max-w-screen-md mx-auto pt-4 px-4 pb-14">
-    <h1 class="text-2xl mt-4 mb-6">原價屋消費爭議記錄</h1>
+    <h1 class="text-2xl mt-4 mb-6">{{ title }}</h1>
     <AppSection v-for="article in defaultArticles" class="mt-4" :article="article" />
   </div>
 </template>
--- pages/labor-law-violations.vue
+++ pages/labor-law-violations.vue
@@ -1,12 +1,22 @@
 <script setup lang="ts">
 import laborLawViolations from '@/assets/json/labor-law-violations.json'
 import LaborLawViolation from '@/components/LaborLawViolation.vue'
+
+const title = '原價屋違反勞基法記錄'
+
+useHead({
+  title,
+  meta: [
+    { name: 'keywords', content: '原價屋,二手屋,瑕疵屋,高價屋' },
+    { name: 'description', content: '原價屋對待勞工的方式，如同對待消費者，如出一轍' }
+  ]
+})
 </script>
 
 <template>
   <div class="max-w-screen-md mx-auto pt-4 px-4 pb-14">
     <h1 class="text-2xl mt-2 mb-6">
-      原價屋違反勞基法記錄
+      {{ title }}
       <a class="ml-2 text-sm"
         href="https://announcement.mol.gov.tw/cmpqry/%E5%8E%9F%E5%83%B9%E5%B1%8B"
         target="_blank">連結</a>
