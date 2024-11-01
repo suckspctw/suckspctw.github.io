9431296cb855c305593860767e289f16a94aad40
suckspctw <suckspctw@gmail.com>
2024-11-01 08:51:45 +0800

    u

--- plugins/vue-gtag.client.ts
+++ plugins/vue-gtag.client.ts
@@ -1,10 +1,24 @@
 import VueGtag, { trackRouter } from 'vue-gtag-next'
 
 export default defineNuxtPlugin(nuxtApp => {
-  nuxtApp.vueApp.use(VueGtag, {
-    property: {
-      id: 'G-R8ER18JW7C'
+  const t = ref(null)
+  onNuxtReady(async () => {
+    const VueGtag = await import('vue-gtag-next')
+    t.value = VueGtag
+
+    nuxtApp.vueApp.use(VueGtag.default, {
+      property: {
+        id: 'G-R8ER18JW7C'
+      },
+      appName: 'suckspctw',
+      isEnabled: false,
+      useDebugger: nuxtApp.payload.config.public.mode !== 'prod',
+      pageTrackerScreenviewEnabled: true
+    });
+    VueGtag.trackRouter(useRouter())
+
+    if (VueGtag) {
+      console.info('vue-gtag plugin installed')
     }
   })
-  trackRouter(useRouter())
 })
